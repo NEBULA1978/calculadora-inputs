e804a5929dcb8e82193f5eeb72e17d339681c57b
NEBULA1978 <rpascualey@gmail.com>
2023-10-06 22:46:44 +0200

    Prueba calculadora

---
+++ bash-crear-html-js/prueba1/html-inputs-variables.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# Prompt for the title of the HTML file
+read -p "Enter the title for the HTML file: " title
+
+# Remove spaces from the title and replace them with underscores
+title=$(echo "$title" | tr ' ' '_')
+
+# Create the HTML file with the provided title
+cat <<EOL > "$title.html"
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+    <title>$title</title>
+    <script src="calculadoramejorada.js" defer></script>
+</head>
+<body>
+    <h1 id="titulo1">Calculadora Cultural</h1>
+    <div class="resultado">
+        Haz calculadora cultural
+        <br /><br />
+        <span style="font-size:1.8em">Calculadora"</span>
+        <ul class="opciones">
+            <li>0) SALIR</li>
+            <li>1) Sumar</li>
+            <li>2) Restar</li>
+            <li>3) Multiplicar</li>
+            <li>4) Dividir</li>
+            <li>5) Obtener el módulo</li>
+            <li>6) Cambiar de base</li>
+            <li>7) Elevar un número</li>
+            <li>8) Para la raíz cuadrada</li>
+            <li>9) Para calcular área de círculo</li>
+            <li>10) Generar número PI con 3 decimales</li>
+        </ul>
+    </div>
+    <div id="entrada1" style="color:red;font-size:1.6em"></div>
+    <div id="entrada2" style="color:red;font-size:1.6em"></div>
+    <form style="margin: auto;text-align: center">
+        <input type="number" id="valor1" placeholder="Primer valor" />
+        <input type="number" id="valor2" placeholder="Segundo valor" />
+        <input type="number" id="opcion" placeholder="Opción" />
+        <input type="button" value="Operacion" onclick="operacion();" />
+    </form>
+    <div id="resultado" style="color:green;font-size:1.6em;margin:auto;text-align:center;">
+        Pulsa para realizar una operación
+    </div>
+</body>
+</html>
+EOL
+
+echo "HTML file ($title.html) created successfully!"
---
+++ clculadora -imputs/calculadoramejorada.js
@@ -0,0 +1,51 @@
+function input_values() {
+    input1 = Number(document.getElementById("valor1").value);
+    input2 = Number(document.getElementById("valor2").value);
+    document.getElementById("entrada1").innerHTML = "Primer Valor → " + input1;
+    document.getElementById("entrada2").innerHTML = "Segundo Valor → " + input2;
+}
+
+function operacion() {
+    var x = Number(document.getElementById("opcion").value);
+    var w = document.getElementById("resultado");
+    if (x == 0) {
+        document.body.innerHTML = "¡Se ha salido!";
+    } else if (x == 1) {
+        input_values();
+        w.innerHTML = input1 + input2;
+    } else if (x == 2) {
+        input_values();
+        w.innerHTML = input1 - input2;
+    } else if (x == 3) {
+        input_values();
+        w.innerHTML = input1 * input2;
+    } else if (x == 4) {
+        input_values();
+        w.innerHTML = input1 % input2;
+    } else if (x == 5) {
+        var base_actual = Number(prompt("Introduce el número"));
+        var base_destino = Number(prompt("Introduce la base de destino"));
+        if ((base_destino > 0) && (base_destino <= 16)) {
+            w.innerHTML = base_actual.toString(base_destino);
+        } else {
+            alert("Valor introducido incorrecto");
+        }
+    } else if (x == 6) {
+        var base = Number(prompt("Introduce base"));
+        var exponente = Number(prompt("Introduce exp"));
+        w.innerHTML = Math.pow(base, exponente);
+    } else if (x == 7) {
+        var base = Number(prompt("Introduce base"));
+        var exponente = Number(prompt("Introduce exp"));
+        w.innerHTML = Math.pow(base, exponente);
+    } else if (x == 8) {
+        var numero = Number(prompt("Introduce el número"));
+        w.innerHTML = Math.sqrt(numero);
+    } else if (x == 9) {
+        var area = Math.pow(Number(prompt("Introduce el radio del círculo")), 2) * Math.PI;
+        w.innerHTML = "El área del círculo es → " + area.toFixed(2) + " cm²";
+    } else if (x == 10) {
+        var pifix = Math.PI.toFixed(4);
+        w.innerHTML = pifix;
+    }
+}
